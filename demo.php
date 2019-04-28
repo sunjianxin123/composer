7c59a49cc23d22ad97c2a331b9614651bae71375
孙建新 <1092248853@qq.com>
2019-04-28 10:57:52 +0800

    study composer

---
+++ demo.php
@@ -0,0 +1,13 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1234
+ * Date: 2019/4/28
+ * Time: 10:37
+ */
+
+require_once './vendor/autoload.php';
+use \Zxx\Test\Tools;
+
+Tools::show();
---
+++ src/Zxx/Test/Tools.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1234
+ * Date: 2019/4/28
+ * Time: 10:30
+ */
+
+namespace Zxx\Test;
+
+class Tools{
+
+    private $name;
+
+    public function __construct()
+    {
+        
+    }
+
+    public static function show()
+    {
+        echo "Hello Word";
+    }
+}
